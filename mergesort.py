d46ad1e64e11ac6e04634bec1e5406886e598016
Nanteqiou <nantenaina.razafindraibe@outlook.com>
2025-06-14 10:54:20 +0200

    Modified the plotting part of mergesort.py to reflect standards(Not sure if the commit for 2 min did work out)

--- mergesort.py
+++ mergesort.py
@@ -37,15 +37,48 @@ def mergeSort(list_to_sort_by_merge):
             list_to_sort_by_merge[i] = right[r]
             r += 1
             i += 1
+    return list_to_sort_by_merge
 
 
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
+base_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+base_length = len(base_list)
+base_positions = range(base_length)
+
+sorted_list = mergeSort(base_list.copy())
+ 
+sorted_length = (len(sorted_list))
+sorted_positions = range(sorted_length)
+
+fig, ax = plt.subplots(1,2, figsize=(12,6),sharey = True)
+
+base_x = np.array(base_positions)
+base_y = np.array(base_list)
+
+ax[0].set_xlabel("Index")
+ax[0].set_ylabel("Numbers")
+ax[0].set_title("Plot of unsorted numbers")
+ax[0].set_yticks(np.arange(20, 100, 10, dtype = int) )
+ax[0].set_ylim(10,100)
+[spine.set_visible(False) for spine in ax[0].spines.values()]
+
+sns.barplot(x=base_x, y=base_y, ax = ax[0], color = "skyblue")
+
+sorted_x = np.array(sorted_positions)
+sorted_y = np.array(sorted_list)
+ax[1].set_title("Plot of sorted numbers")
+ax[1].set_xlabel("Index")
+ax[1].set_ylabel("Numbers")
+ax[1].set_yticks(np.arange(20, 100, 10, dtype = int))
+ax[1].set_ylim(10, 100)
+[spine.set_visible(False) for spine in ax[1].spines.values()]
+
+sns.barplot(x=sorted_x, y=sorted_y, ax = ax[1], color ="yellow")
+
 plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+
+
+
